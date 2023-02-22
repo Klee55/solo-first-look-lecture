5e2c7be5518e0b528566f7ce9079ffb37677b801
Khue Lee <khuelee55@gmail.com>
2023-02-21 20:30:25 -0600

    added authentication

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -7,13 +7,19 @@ router.get('/', (req, res) => {
     console.log('/pet GET route');
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
-    let queryText = `SELECT * FROM "pets"`;
-    pool.query(queryText).then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log(error);
-        res.sendStatus(500);
-    });
+    if (req.isAuthenticated()) {
+        let queryText = 
+            `SELECT * FROM "pets"
+             WHERE "pets"."user_id" = $1`;
+        pool.query(queryText, [req.user.id]).then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 // This route *should* add a pet for the logged in user
@@ -23,7 +29,7 @@ router.post('/', (req, res) => {
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
     res.sendStatus(200);
-    
+
 });
 
 module.exports = router;
